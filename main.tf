7ce90db19dfdecb2bcc160012d3c0affa9aa5707
Alexandre Andrade <alexandrade@google.com>
2026-03-19 13:22:42 -0300

    fix: add cost-center label to GCE instance

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@ resource "google_compute_instance" "default" {
   zone         = var.zone
   name         = var.instance_name
   machine_type = var.machine_type
+  labels = {
+    cost-center = var.cost_center
+  }
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
