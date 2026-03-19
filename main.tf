2d15b66ffc138e744a712d7e87d6d3571d4bc786
Alexandre Andrade <alexandrade@google.com>
2026-03-19 13:22:08 -0300

    feat: add main.tf for GCE instance

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "google_compute_instance" "default" {
+  project      = var.project_id
+  zone         = var.zone
+  name         = var.instance_name
+  machine_type = var.machine_type
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
