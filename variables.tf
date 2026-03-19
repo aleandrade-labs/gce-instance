7da2611c0f7dcc8d978c91540d26e0fd3422bfbf
Alexandre Andrade <alexandrade@google.com>
2026-03-19 13:22:15 -0300

    feat: add variables.tf for GCE instance

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "project_id" {
+  description = "The ID of the Google Cloud project."
+  type        = string
+}
+
+variable "zone" {
+  description = "The zone for the GCE instance."
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "instance_name" {
+  description = "The name of the GCE instance."
+  type        = string
+  default     = "gce-instance-example"
+}
+
+variable "machine_type" {
+  description = "The machine type of the GCE instance."
+  type        = string
+  default     = "e2-medium"
+}
